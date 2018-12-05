fcd23c99fc535156fdcdd9fbd3ef4bd1dd685195
maoxiaoxiao <maoxiaoxiao@vipkid.com.cn>
2018-12-05 20:22:35 +0800

    update

--- src/comps/CountDown.js
+++ src/comps/CountDown.js
@@ -8,9 +8,11 @@ const Progress = styled.div.attrs({
   left: 0;
   right: 0;
   bottom: 0;
+  margin: 0 auto;
   width: ${props => props.width};
   background-color: red;
   height: 10px;
+  transition: width 1s linear;
 `;
 
 
--- src/comps/SelectAnswer.js
+++ src/comps/SelectAnswer.js
@@ -1,10 +1,59 @@
 import React from 'react';
 import BaseComp from './BaseComp';
 import Progress from './CountDown';
+import styled from 'styled-components';
+
+const BoxStyle = styled.div`
+  width: 660px;
+`;
+
+const TitleStyle = styled.div`
+  margin-bottom: 38px;
+  height: 40px;
+  line-height: 37px;
+  text-align: center;
+  font-size: 30px;
+  font-weight: bold;
+  color: #FFF;
+`;
+
+const OptionPanelStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const OptionStyle = styled.div.attrs({
+  boxShadow: props => {
+    if(props.result === null){
+      return "0 0 0 6px rgba(152, 136, 207, 1)"
+    }
+    return props.result ? " 0 0 0 6px rgba(83, 227, 189, 1)" : " 0 0 0 6px rgba(252, 97, 97, 1)";
+  }
+})`
+  width: 180px;
+  height: 135px;
+  border-radius: 20px;
+  margin-right: 60px;
+  background-color: #FFF;
+  cursor: pointer;
+  transition: all 0.2s linear;
+  &:last-child{
+    margin-right: 0;
+  }
+  &:hover{
+    box-shadow: ${props => props.boxShadow};
+  }
+`;
+
+const ResultOptionStyle = styled(OptionStyle)`
+  box-shadow: ${props=>props.result ? " 0 0 0 6px rgba(83, 227, 189, 1)" : " 0 0 0 6px rgba(252, 97, 97, 1)"}
+`;
 class SelectAnswer extends React.Component{
   constructor(props){
     super(props);
     this.state ={
+      title: '',
       options: [{
         name: 'A',
         right: false
@@ -15,33 +64,47 @@ class SelectAnswer extends React.Component{
         name: 'C',
         right: false
       }],
-      countDown: 10
+      answer: null,
+      countDown: 10,
+      time: 10
     }
   }
   componentDidMount(){
-
     // 倒计时处理
     let time = this.state.countDown;
     let timer = setInterval(()=>{
-      console.log(time);
       time--;
+      this.setState({
+        time
+      });
+
       if(time === 0){
+        this.setState({
+          time
+        });
         clearInterval(timer);
         console.log('done');
       }
     }, 1000);
   }
   handleClick = (option)=>{
-    alert(option.name);
+    this.setState({
+      answer: option.name
+    })
   };
   render(){
+    const ToggleOptionStyle = this.state.answer === null ? OptionStyle:ResultOptionStyle;
     const result = this.state.options.map((opt, index)=>{
-      return (<div key={index} onClick={()=>{this.handleClick(opt)}}>{opt.name}:{opt.right ? '1' : '0'}</div>)
+      const result = this.state.answer ? opt.right : null;
+      return (<ToggleOptionStyle key={index} result={result} onClick={()=>{this.handleClick(opt)}}>{opt.name}:{opt.right ? '1' : '0'}</ToggleOptionStyle>);
     });
-    return <div>
-      {result}
-      {Progress}
-    </div>
+    return <BoxStyle>
+      <TitleStyle >which food grown in Jennie’s farm</TitleStyle>
+      <OptionPanelStyle>
+        {result}
+      </OptionPanelStyle>
+      <Progress currentTime={this.state.time} totalTime={this.state.countDown}/>
+    </BoxStyle>
 
   }
 }
